7fefa019be0315d2d4cae7a1f210bca94d783e52
crazydog54088 <95664578+crazydog54088@users.noreply.github.com>
2022-01-10 22:45:51 +0800

    Update Student.java

--- src/User/Student.java
+++ src/User/Student.java
@@ -4,13 +4,13 @@ import Other.Lesson;
 
 public class Student extends User{
 	
-	private int lessonNumber;
+	private int TestNumber;
 	
 	public Student(int Id) {
 		super(Id);
 	}
 	
-	public String[] getLessonList() {
+	public String[] getTestList() {
 		String strStudentInfo = Other.IO.InputById(filePath + "CourseRegistration.txt", this.id);
 		return strStudentInfo.split(",")[1].split("#");
 	}
@@ -19,14 +19,14 @@ public class Student extends User{
 		String[] strList = Other.IO.InputById(filePath + "CourseRegistration.txt", this.id).split(",")[1].split("#");
 		String[] ScoreList = new String[strList.length];
 		
-		// 資料數 = 年份 + 課程
+		
 		int intDataNumber = 0;
 		for (int index = 0; index < strList.length; index += 2) {
-			String[] LessonList = strList[index + 1].split("&");
+			String[] TestList = strList[index + 1].split("&");
 			String strScore = "";
-			intDataNumber += LessonList.length + 1;
-			for (String LessonId: LessonList) {
-				String InputData = Other.IO.InputById(filePath + "Score\\" + LessonId + ".txt", Integer.parseInt(strList[index]));
+			intDataNumber +=TestList.length + 1;
+			for (String TestId: TestList) {
+				String InputData = Other.IO.InputById(filePath + "Score\\" + TestId + ".txt", Integer.parseInt(strList[index]));
 				String[] InputId = InputData.split(",")[1].split("&");
 				String[] InputScore = InputData.split(",")[2].split("&");
 				for (int i = 0; i < InputId.length; i++) {
@@ -37,24 +37,24 @@ public class Student extends User{
 			ScoreList[index] = strList[index];
 			ScoreList[index + 1] = strScore;
 		}
-		this.lessonNumber = intDataNumber;
+		this.TestNumber = intDataNumber;
 		
 		return ScoreList;
 	}
 	
 	public String[][] getAllScore(){
-		String[] LessonList = this.getLessonList();
+		String[] TestList = this.geTestList();
 		String[] ScoreList = this.getScoreList();
 		
-		String[][] outputData = new String[this.lessonNumber][3];
+		String[][] outputData = new String[this.TestNumber][3];
 		
-		for (int index = 0, i = 0; i < LessonList.length; i += 2) {
-			String[] LessonInSemester = LessonList[i + 1].split("&");
-			String[] ScoreInSemester = ScoreList[i + 1].split("&");
-			outputData[index++] = new String[] {LessonList[i], LessonInSemester.length + "", "dataInfo"};
+		for (int index = 0, i = 0; i < TestList.length; i += 2) {
+			String[] TestInSemester = TestList[i + 1].split("&");
+			String[] ScoreInSemester =TestList[i + 1].split("&");
+			outputData[index++] = new String[] {LessonList[i], TestnInSemester.length + "", "dataInfo"};
 			
-			for (int j = 0; j < LessonInSemester.length; j++) {
-				outputData[index++] = new String[] {LessonInSemester[j], new Lesson(Integer.parseInt(LessonInSemester[j])).name, ScoreInSemester[j]};
+			for (int j = 0; j < TestInSemester.length; j++) {
+				outputData[index++] = new String[] {TestInSemester[j], new Lesson(Integer.parseInt(LessonInSemester[j])).name, ScoreInSemester[j]};
 			}
 		}
 		
